dfbbf45b5c20c5b41d54131ac7924d862ea01606
lvmendess <liviamendes1703@gmail.com>
2024-09-26 22:32:53 -0300

    Co-authored-by: Paulo Moura <pmmc026@users.noreply.github.com>

--- CacheMapper.java
+++ CacheMapper.java
@@ -4,29 +4,37 @@ public class CacheMapper {
     private long n; // tamanho da memória
     private int p; // num de palavras no bloco
     private int l; //número total de linhas da memória cache
-    private int v; //número de blocos do conjunto
+    private int v; //quantidade de blocos/linhas no conjunto
     private final int wordSize = 4;
     private String[] e; // endereços de memória
 
     public CacheMapper(ArrayList<String[]> linhas) {
         this.n = StringToInt(linhas.get(0)[0]);
+        bytesAddress(n);
         this.p = StringToInt(linhas.get(1)[0]);
         this.l = StringToInt(linhas.get(2)[0]);
         this.v = StringToInt(linhas.get(3)[0]);
         this.e = linhas.get(4);
+        System.out.println(dataBitSize());
     }
 
     public int StringToInt(String s){
         int number = 0;
         try{
-            number = Integer.valueOf(s);
+            number = Integer.parseInt(s);
         }catch(NumberFormatException e){
             System.out.println("Invalid number");
         }
         return number;
     }
 
-    
+    public int bytesAddress(long n){
+        return (int)(Math.log(n)/Math.log(2));
+    }
+
+    public int dataBitSize(){
+        return bytesAddress(wordSize * p);
+    }
 
     
 }
